2cc9ffb8e7c910111d012af1259ed537fd5131cf
Renan Couto <renancouto@gmail.com>
2015-10-08 19:41:27 -0300

    added - location finder

--- src/js/components/location.js
+++ src/js/components/location.js
@@ -3,26 +3,36 @@
 
 var directionsService = new google.maps.DirectionsService()
 
-function getDirections(val) {
+function render(route) {
+  $('#location-route').addClass('show').html(route)
+}
+
+function processRoute(routes) {
+  var markup = [ '<ol>' ]
+
+  routes.steps.forEach(function (step) {
+    markup.push('<li>' + step.instructions + '</li>')
+  })
+
+  markup.push('</ol>')
+  return markup.join('')
+}
+
+function getDirections(origin) {
   var request = {
-    origin: val,
     destination: 'Avenida Maringá, 1995 Londrina - Paraná',
+    origin: origin,
     travelMode: google.maps.TravelMode.DRIVING
   }
 
-  directionsService.route(request, function(result, status) {
-    if (google.maps.DirectionsStatus.OK && status !== 'ZERO_RESULTS') {
-      console.log(result.routes[0].legs[0])
-    }
+  directionsService.route(request, function (result, status) {
+    render(google.maps.DirectionsStatus.OK && status !== 'ZERO_RESULTS'
+      ? processRoute(result.routes[0].legs[0])
+      : 'Nenhuma rota encontrada'
+    )
   })
 }
 
 $('#location-field').on('blur', function () {
-  var val = this.value
-
-  if (val.length < 3) {
-    return
-  }
-
-  getDirections(val)
+  getDirections(this.value)
 })
